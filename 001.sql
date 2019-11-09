9a0b7b71a110648ac4cdf6c8d52a5959bfe1596b
luskyqi <32703906+15313205676@users.noreply.github.com>
2019-11-10 00:54:52 +0800

    Update 001.sql

--- 001.sql
+++ 001.sql
@@ -1,8 +1,28 @@
 # 进入mysql
 mysql -u root -p 
+
 # 创建新用户
 # CREATE USER 'username'@'host' IDENTIFIED BY 'password';
 # username：你将创建的用户名
 # host：指定该用户在哪个主机上可以登陆，如果是本地用户可用localhost，如果想让该用户可以从任意远程主机登陆，可以使用通配符%
 # password：该用户的登陆密码，密码可以为空，如果为空则该用户可以不需要密码登陆服务器
 CREATE USER 'dog'@'localhost' IDENTIFIED BY '123456';
+
+#授权
+#GRANT privileges ON databasename.tablename TO 'username'@'host'
+#privileges：用户的操作权限，如SELECT，INSERT，UPDATE等，如果要授予所的权限则使用ALL
+#databasename：数据库名
+#tablename：表名，如果要授予该用户对所有数据库和表的相应操作权限则可用*表示，如*.*
+mysql> GRANT ALL ON *.* TO 'dog'@'localhost';
+Query OK, 0 rows affected (0.00 sec)
+
+#查询权限
+mysql> show grants for dog@'localhost';
++-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+| Grants for dog@localhost                                                                                                                                                                                                                                                                                                                                                                                                                                                                        |
++-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+| GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE, CREATE ROLE, DROP ROLE ON *.* TO `dog`@`localhost`                                                                                                           |
+| GRANT APPLICATION_PASSWORD_ADMIN,AUDIT_ADMIN,BACKUP_ADMIN,BINLOG_ADMIN,BINLOG_ENCRYPTION_ADMIN,CLONE_ADMIN,CONNECTION_ADMIN,ENCRYPTION_KEY_ADMIN,GROUP_REPLICATION_ADMIN,INNODB_REDO_LOG_ARCHIVE,PERSIST_RO_VARIABLES_ADMIN,REPLICATION_APPLIER,REPLICATION_SLAVE_ADMIN,RESOURCE_GROUP_ADMIN,RESOURCE_GROUP_USER,ROLE_ADMIN,SERVICE_CONNECTION_ADMIN,SESSION_VARIABLES_ADMIN,SET_USER_ID,SYSTEM_USER,SYSTEM_VARIABLES_ADMIN,TABLE_ENCRYPTION_ADMIN,XA_RECOVER_ADMIN ON *.* TO `dog`@`localhost` |
++-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+2 rows in set (0.00 sec)
+
